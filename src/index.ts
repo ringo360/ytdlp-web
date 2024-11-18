342b139164ff88770aa6f0ef9e4f10681f024ffc
ringo360 <ringoxd@ringoxd.dev>
2024-11-18 11:39:42 +0900

    progress(logger, html input)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/bun';
+import { logger} from 'hono/logger'
 import api from './api';
 
 const app = new Hono();
+app.use(logger())
 
 app.use('/files/*', serveStatic({ root: './' }));
 app.use('/', serveStatic({ root: './html' }));
